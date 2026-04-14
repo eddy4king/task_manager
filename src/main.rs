650e23b636756eb3661293ef15444635e78d1196
Edosa Ilawe Joseph <eddyking0@gmail.com>
2026-04-14 11:26:02 +0200

    feat: connect rust app to postgres via sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    println!("Connecting to : {}", database_url);
+
+    let _pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+    println!("Database connected successfully!");
 }
