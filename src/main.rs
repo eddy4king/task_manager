9ca99ceed5cbd7a35e537f573b8320104f5026be
Edosa Ilawe Joseph <eddyking0@gmail.com>
2026-04-14 17:23:30 +0200

    feat: add AppState and wire db pool into router

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 mod db;
 mod models;
 mod handlers;
+mod state;
 
 use dotenv::dotenv;
 use axum::{routing::get, Router};
 use handlers::health_check;
+use state::AppState;
+
 
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    db::connect().await;
+    let pool= db::connect().await;
+    let state = AppState{db: pool};
 
     let app = Router::new()
-            .route("/health", get (health_check));
+            .route("/health", get (health_check))
+            .with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
             .unwrap();
