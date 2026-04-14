a1f182cb8b7e4d4a870c007f10b35c9ad7d62a62
Edosa Ilawe Joseph <eddyking0@gmail.com>
2026-04-14 17:01:51 +0200

    feat: add axum router and health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 mod db;
 mod models;
+mod handlers;
 
 use dotenv::dotenv;
+use axum::{routing::get, Router};
+use handlers::health_check;
 
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     db::connect().await;
+
+    let app = Router::new()
+            .route("/health", get (health_check));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+            .await
+            .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
     
